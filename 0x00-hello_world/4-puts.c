a42900007f1ac8301c15266859214d11a352926d
jollysinao <jollysinao@gmail.com>
2023-04-20 11:14:47 -0700

    4putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 * main - entry point
-*
-* Return: Always 0 (success)
+* Return : Always 0(success)
 */
 int main(void)
 {
-----------  puts("Programming is like building a multilingual puzzle\n");
------------'return' (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
